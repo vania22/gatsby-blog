d3cf5c829c0553fa05611d235ed4d4c403737e0a
Ivan Krupskyi <krupskiy111@gmail.com>
2020-12-24 00:35:17 +0000

    Fetching markdown posts and displaying them

---
+++ src/components/Post.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { Link } from "gatsby"
+import { Card, CardTitle, CardText, CardSubtitle, CardBody } from "reactstrap"
+
+const Post = ({ title, author, path, date, body }) => {
+  return (
+    <Card>
+      <CardBody>
+        <CardTitle>
+          <Link to={path}>{title} </Link>
+        </CardTitle>
+        <CardSubtitle>
+          <span className="text-info mr-2">{date}</span>
+          by
+          <span className="text-info ml-2">{author}</span>
+        </CardSubtitle>
+        <CardText>{body}</CardText>
+        <Link to={path} className="btn btn-outline-primary float-right">
+          Read more
+        </Link>
+      </CardBody>
+    </Card>
+  )
+}
+
+export default Post
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,53 @@
 import React from "react"
+import { graphql, StaticQuery } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Post from "../components/Post"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
+    <StaticQuery
+      query={indexQuery}
+      render={data => {
+        return (
+          <div key="">
+            {data.allMarkdownRemark.edges.map(({ node }) => (
+              <Post
+                title={node.frontmatter.title}
+                author={node.frontmatter.author}
+                path={node.frontmatter.path}
+                date={node.frontmatter.date}
+                body={node.excerpt}
+                key={node.id}
+              />
+            ))}
+          </div>
+        )
+      }}
+    />
   </Layout>
 )
+
+const indexQuery = graphql`
+  query {
+    allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "MMM Do YYYY")
+            author
+            path
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
